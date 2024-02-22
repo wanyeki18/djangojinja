e6ca9eaed9abfde1a50bae712598f9026c1a4b68
wanyeki18 <shawanyeki@gmail.com>
2024-02-22 10:18:18 +0300

    Initial commit

---
+++ jinjaApp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+def gallery(request):
+    return render(request, 'gallery.html')
